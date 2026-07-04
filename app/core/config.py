0a4d8986a6f83d29977eb0aa5e91b4a8d4010d8b
rajmohan-14 <rajmohan14k@gmail.com>
2026-07-04 11:08:41 +0530

    updated env and requirements

--- app/core/config.py
+++ app/core/config.py
@@ -9,6 +9,8 @@ class Settings(BaseSettings):
     ALGORITHM: str = "HS256"
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 30
     REFRESH_TOKEN_EXPIRE_DAYS: int = 7
+    GROQ_API_KEY: str
+    GROQ_MODEL: str = "llama3-8b-8192"
 
     class Config:
         env_file = ".env"
